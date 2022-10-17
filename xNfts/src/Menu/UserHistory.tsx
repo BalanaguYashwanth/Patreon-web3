b7c66457954032dbdf388bd45404353adc45fa51
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2022-10-17 10:13:52 +0530

    updated history

--- xNfts/src/Menu/UserHistory.tsx
+++ xNfts/src/Menu/UserHistory.tsx
@@ -1,10 +1,75 @@
 import React from 'react'
-import ReactXnft, { Text, View } from "react-xnft";
+import ReactXnft, { Button, Text, View } from "react-xnft";
 
-export const UserHistory = () =>{
-    return(
-        <View style={{textAlign:'center'}}>
-            <Text> Currently working on User History </Text>
+export const UserHistory = () => {
+  return (
+    <View
+      style={{
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+      <View>
+        <Button style={{margin:'10px'}}> Gari </Button>
+        
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItem: "center",
+            justifyContent: "space-between",
+            margin : '0 auto', 
+            gap: '25px',
+          }}
+        >
+          <View>
+            <Text> Amount of stake </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
+          <View>
+            <Text> Amount of unStake </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
+          <View>
+            <Text> Rewards Claimed </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
         </View>
-    )
-}
+      </View>
+      <View>
+        <Button style={{margin:'10px'}}> Panda </Button>
+       
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItem: "center",
+            justifyContent: "space-between",
+            margin : '0 auto', 
+            gap: '25px',
+          }}
+        >
+          <View>
+            <Text> Time of stake </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
+          <View>
+            <Text> Time of unStake </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
+          <View>
+            <Text> Time of claim </Text>
+            <Text> 0 </Text>
+            <Button> View TX </Button>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
